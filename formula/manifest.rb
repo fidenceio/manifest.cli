a1734436b174b7e0470d281d8b0d77aa82420a38
William Cervin <william.cervin@lamppost.ventures>
2026-05-26 22:53:06 -0500

    Update Homebrew formula to v49.0.2

--- formula/manifest.rb
+++ formula/manifest.rb
@@ -1,8 +1,8 @@
 class Manifest < Formula
   desc "A powerful CLI tool for managing manifest files, versioning, and repository operations with trusted timestamp verification"
   homepage "https://github.com/fidenceio/manifest.cli"
-  url "https://github.com/fidenceio/manifest.cli/archive/refs/tags/v49.0.1.tar.gz"
-  sha256 "570e91c3eeeaafe22a45b84bc8168455a80699ff923cf4e8d9c8acfcd3b90443"
+  url "https://github.com/fidenceio/manifest.cli/archive/refs/tags/v49.0.2.tar.gz"
+  sha256 "a8c266ae33c9759e845df51673393277c5f1f3a577c15c0aa57bbd7b8fb66ac0"
   license "MIT"
   head "https://github.com/fidenceio/manifest.cli.git", branch: "main"
 
